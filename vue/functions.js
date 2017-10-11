615a34242951e5aaeb9e478aa66c6579576cfdf0
shindousaigo <597917872@qq.com>
2017-10-11 20:45:35 +0800

    add new file

---
+++ vue/functions.js
@@ -0,0 +1,14 @@
+function makeMap(str, expectsLowerCase) {
+  // 创建一个原型为null的空对象
+  var map = Object.create(null);
+  var list = str.split(",");
+  for (
+    var i = 0; i < (list.length); i++) {... }
+  return expectsLowerCase ?
+    (
+      function(val) {... }
+    ) :
+    (
+      function(val) {... }
+    );
+}
---
+++ vue/index.md
@@ -0,0 +1,22 @@
+```
+
+function makeMap (str, expectsLowerCase) 
+{
+var map = Object.create (null) ;
+var list = str.split (",") ;
+for (
+var i = 0
+ ; i < (list.length) ; i ++) 
+{...}
+return expectsLowerCase 
+? (
+function (val) 
+{...}
+)
+: (
+function (val) 
+{...}
+)
+ ;
+}
+```
